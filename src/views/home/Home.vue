bf832a32c020b25bb5a73ec20c153057074d8328
wwwxf123 <1710926597@qq.com>
2021-09-27 08:51:10 +0800

    dome-login

---
+++ src/views/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+<!--    主页写自己的内容-->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "../login/Login";
+export default {
+  name: 'Home',
+  components: {
+      Login
+  }
+}
+</script>
